55ccf3cadebcf4d47b26a2c7fd6db1efbcc1749d
Kibnakamoto <kibnakanoto@protonmail.com>
2023-04-15 22:10:11 -0400

    update files of add_to_pico

---
+++ root.sh
@@ -0,0 +1,3 @@
+# add required files into root (This is for developer)
+cd pico-ducky/
+cp /boot.py duckyinpython.py code.py webapp.py wsgiserver.py ../add_to_pico/root/
--- setup.py
+++ setup.py
@@ -4,28 +4,39 @@
 Date: Apr 8, 2023
 """
 
+import os
+import shutil
+from time import sleep
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 # Setup the Microcontoller for hacking (hacking usb) or default mode (Normal MicroController)
 class Setup(QMainWindow):
     """ Default Class Initializer """
-    def __init__(self):
+    def __init__(self, pico_path):
         super().__init__()
         self.hack_mode = True
+        self.path = os.path.dirname(os.path.realpath(__file__)) # get path of the application folder
+        self.add_path = self.path + "/add_to_pico/"
+        self.pico_path = pico_path
     
     # enable hacking mode
     def enable_h(self) -> None:
+        # upload the files onto the microcontoller
         self.hack_mode = True
 
-        # upload the files onto the microcontoller
-        # TODO: ^
+        # add uf2 file
+        shutil.copy("{self.add_path}/uf2.uf2", self.pico_path) # copy file to Downloads folder
+        sleep(2.0) # wait for the uf2 file to be uploaded and microcontroller to reboot
+        
+        
 
     # disable hacking mode
     def disable_h(self) -> None:
-        pass
+        # remove the uploaded files from the microcontroller
+        self.hack_mode = False
 
 
 app = QApplication([])
-w = Setup()
+w = Setup("/media/kibnakamoto/RPI-RP2")
 w.show()
 app.exec_()
